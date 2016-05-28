2253201f84cdf78808f5ec8138ef15f90289aae5
CiobanuMarcu <ciobanumarcu@yahoo.ro>
2016-05-28 12:25:12 +0300

    - adaugat dump schema

---
+++ db/coderdojo_db_1_0.sql
@@ -0,0 +1,137 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: coderdojo
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_utilizator` varchar(30) DEFAULT NULL,
+  `text` varchar(250) DEFAULT NULL,
+  `data` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_feedback_fk_idx` (`id_utilizator`),
+  CONSTRAINT `user_feedback_fk` FOREIGN KEY (`id_utilizator`) REFERENCES `utilizator` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feedback`
+--
+
+LOCK TABLES `feedback` WRITE;
+/*!40000 ALTER TABLE `feedback` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feedback` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inscrieri`
+--
+
+DROP TABLE IF EXISTS `inscrieri`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inscrieri` (
+  `id_inscriere` int(11) NOT NULL,
+  `id_utilizator` varchar(30) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  PRIMARY KEY (`id_inscriere`),
+  KEY `user_fk_idx` (`id_utilizator`),
+  KEY `session_fk_idx` (`data`),
+  CONSTRAINT `session_fk` FOREIGN KEY (`data`) REFERENCES `sesiune` (`data`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_fk` FOREIGN KEY (`id_utilizator`) REFERENCES `utilizator` (`username`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inscrieri`
+--
+
+LOCK TABLES `inscrieri` WRITE;
+/*!40000 ALTER TABLE `inscrieri` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inscrieri` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sesiune`
+--
+
+DROP TABLE IF EXISTS `sesiune`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sesiune` (
+  `data` date NOT NULL,
+  `ora` time DEFAULT NULL,
+  `locatia` varchar(100) DEFAULT NULL,
+  `descriere` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`data`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sesiune`
+--
+
+LOCK TABLES `sesiune` WRITE;
+/*!40000 ALTER TABLE `sesiune` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sesiune` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilizator`
+--
+
+DROP TABLE IF EXISTS `utilizator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilizator` (
+  `username` varchar(30) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `nume` varchar(45) DEFAULT NULL,
+  `prenume` varchar(45) DEFAULT NULL,
+  `telefon` int(10) DEFAULT NULL,
+  `parola` varchar(45) DEFAULT NULL,
+  `data_inregistrarii` date DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilizator`
+--
+
+LOCK TABLES `utilizator` WRITE;
+/*!40000 ALTER TABLE `utilizator` DISABLE KEYS */;
+INSERT INTO `utilizator` VALUES ('demo565',NULL,'x','z',0,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `utilizator` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-28 12:23:25

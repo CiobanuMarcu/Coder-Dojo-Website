d610f9879c98d943027f5801752b31c916731990
CristiMachine25 <cristimachine25@gmail.com>
2016-09-10 11:08:20 +0300

    - added login page back

---
+++ src/login/login.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Login to Coder Dojo Iasi</title>
+        <link rel="stylesheet" href="../css/main.css">
+    </head>
+<body>
+    <form class='login_form' method="post" action="../logic/login_logic.php">
+        <h2>Login</h2>
+        <input type="text" name="username"/>
+        <input type="password" name="parola"/>
+        <input type="submit" id="submit" name="submit" value="Login"/>
+        <div class="field-set">
+                <label>username</label>
+                <input type="text" name="username"/>
+        </div>
+        <div class="field-set">
+            <label>password</label>
+            <input type="password" name="password"/>
+        </div>
+    </form>
+</body>
+</html> 

840252ac43bde038d1f445c98a9662f42d5fbc2a
CiobanuMarcu <ciobanumarcu@yahoo.ro>
2016-09-24 11:11:02 +0300

    - renuntat lucru cu myView/MVC

---
+++ src/view/meniu.php
@@ -0,0 +1,18 @@
+<html>
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MJ
+ * Date: 17-Sep-16
+ * Time: 2:01 PM
+ */
+?>
+    <body>
+        <div id="meniu">
+            <!-- <img src="http://www.bobses.eu/wp-content/uploads/2015/09/coderdojo.jpg" width="400" height="180"/> -->
+        </div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </body>
+</html>

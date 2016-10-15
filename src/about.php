faff35358435fbf4eefa07af1715a841d2d68db8
CristiMachine25 <cristimachine25@gmail.com>
2016-10-15 12:58:14 +0300

    facut meniu

---
+++ src/about.php
@@ -0,0 +1,9 @@
+<p>Testing about page</p>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: crist
+ * Date: 15.10.2016
+ * Time: 10:32
+ */
+
--- src/view/meniu.php
+++ src/view/meniu.php
@@ -1,5 +1,5 @@
 <html>
-    <link rel="stylesheet" type="text/css" href="../css/main.css">
+<link rel="stylesheet" type="text/css" href="../css/main.css">
 <?php
 /**
  * Created by PhpStorm.
@@ -8,11 +8,19 @@
  * Time: 2:01 PM
  */
 ?>
-    <body>
-        <div id="meniu">
-            <!-- <img src="http://www.bobses.eu/wp-content/uploads/2015/09/coderdojo.jpg" width="400" height="180"/> -->
-        </div>
-        <div class="box"></div>
-        <div class="box"></div>
-    </body>
+<body>
+
+
+    <table class="TableClass">
+
+        <tr>
+               
+            <th><a href="../login.php">Home</a></th>
+                 
+            <th><a href="../about.php">About</a></th>  
+
+        </tr>
+    </table>
+
+</body>
 </html>

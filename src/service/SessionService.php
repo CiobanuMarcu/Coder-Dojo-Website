fcf57d1d37e3144d6fbf5ff1c011cc43da689083
CristiMachine25 <cristimachine25@gmail.com>
2017-08-12 12:59:38 +0300

    Fixat bug

--- src/service/SessionService.php
+++ src/service/SessionService.php
@@ -17,7 +17,7 @@ class SessionService{
     static function get_next_sessions() {
         include ('DatabaseManager.php');
         $connection = getConnection();
-        $query = sprintf ("SELECT * FROM coderdojo.sesiune WHERE data > CURRENT_DATE
+        $query = sprintf ("SELECT * FROM sesiune WHERE data > CURRENT_DATE
                           OR (data = CURRENT_DATE AND CURRENT_TIME < ora - INTERVAL 1 hour)");
         $sessions = array();
         if ($result = mysqli_query($connection, $query)){
@@ -36,7 +36,7 @@ class SessionService{
     static function subscribe_user_to_session ($data, $username){
         include ('DatabaseManager.php');
         $connection = getConnection();
-        $query = sprintf("INSERT INTO coderdojo.inscrieri (id_utilizator, data) VALUES('%s', '%s')",
+        $query = sprintf("INSERT INTO inscrieri (id_utilizator, data) VALUES('%s', '%s')",
             mysqli_real_escape_string($connection, $username),
             mysqli_real_escape_string($connection, $data)
         );
@@ -51,7 +51,7 @@ class SessionService{
     static function unsubscribe_user_to_session ($data, $username){
         include('DatabaseManager.php');
         $connection = getConnection();
-        $query = sprintf("DELETE FROM coderdojo.inscrieri WHERE id_utilizator = '%s' AND data = '%s'",
+        $query = sprintf("DELETE FROM inscrieri WHERE id_utilizator = '%s' AND data = '%s'",
             mysqli_real_escape_string($connection, $username),
             mysqli_real_escape_string($connection, $data)
         );
@@ -64,7 +64,7 @@ class SessionService{
 
     static function is_subscribed ($data, $user_id){
         $connection = getConnection();
-        $query = sprintf("SELECT * FROM coderdojo.inscrieri WHERE data = '%s' AND id_utilizator = '%s'", $data, $user_id);
+        $query = sprintf("SELECT * FROM inscrieri WHERE data = '%s' AND id_utilizator = '%s'", $data, $user_id);
         $result = mysqli_query($connection, $query);
         $rows = mysqli_num_rows($result);
         closeConnection($connection);
